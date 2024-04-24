2596bc4ba220ed6c3180471adfa9d3f49e90c40b
LEEMINKUK <158013587+LEEMINKUK@users.noreply.github.com>
2024-04-24 18:10:38 +0900

    commit

---
+++ chap00-test/src/main/java/com/ohgiraffers/test/Chap00TestApplication.java
@@ -0,0 +1,13 @@
+package com.ohgiraffers.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chap00TestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Chap00TestApplication.class, args);
+	}
+
+}
---
+++ chap00-test/src/test/java/com/ohgiraffers/test/Chap00TestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ohgiraffers.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Chap00TestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
